da8af5528e05805dc43509fde55eea9741faa0bc
LabyKreative <labykreative@yahoo.com>
2023-11-01 10:18:49 +0000

    Use user locale...

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -57,7 +57,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index() -> str:
     """Handles route."""
     return render_template("5-index.html")
