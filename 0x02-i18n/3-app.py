b7d6ce3780f88d378635ea312a6ddfb28f915a92
LabyKreative <labykreative@yahoo.com>
2023-11-01 10:14:00 +0000

    Parametrize templates 🤣

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 """Flask App."""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
