059a206bc32f956ade795f3604df6060361683b6
LabyKreative <labykreative@yahoo.com>
2023-11-30 01:07:36 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,29 @@
+// A script that connects to Redis server and print the passed in args in hash
+import { createClient, print } from 'redis';
+
+const redisClient = createClient();
+
+redisClient.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', function(error) {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+// hash key-values in list
+redisClient.hset('HolbertonSchools', 'Portland', '50', print);
+redisClient.hset('HolbertonSchools', 'Seattle', '80', print);
+redisClient.hset('HolbertonSchools', 'New York', '20', print);
+redisClient.hset('HolbertonSchools', 'Bogota', '20', print);
+redisClient.hset('HolbertonSchools', 'Cali', '40', print);
+redisClient.hset('HolbertonSchools', 'Paris', '2', print);
+
+// gets all elements stored in the list
+redisClient.hgetall('HolbertonSchools', function (error, result) {
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  console.log(result);
+});
