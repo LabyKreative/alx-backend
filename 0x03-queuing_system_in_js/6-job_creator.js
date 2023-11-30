a0c29f8128c0e9603503c8c16393bda202cc0be5
LabyKreative <labykreative@yahoo.com>
2023-11-30 01:50:51 +0000

    Create the Job creator: intersting

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -4,8 +4,8 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const notification = {
-  'phoneNumber': '8012345678',
-  'message': 'Verify your account with this code'
+  'phoneNumber': '4153518780',
+  'message': 'This is the code to verify your account'
 }
 
 const job = queue.create('push_notification_code', notification).save(function (error) {
