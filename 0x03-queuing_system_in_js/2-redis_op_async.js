954ea4843f8ebbc13764d3238dc62cd2dc53cd97
LabyKreative <labykreative@yahoo.com>
2023-11-30 00:59:42 +0000

    Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+// A script that connects to the Redis server and print the passed in args
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function (err) {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+};
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const result = await get(schoolName).catch((error) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+  console.log(result);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
